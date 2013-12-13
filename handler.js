988d416a2b4154366c449776a239117252d7a8f9
Zeph Lafassett <zeph@lafassett.com>
2013-12-12 19:38:36 -0800

    adding post request to db

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var qs = require('querystring');
 var db = mongoose.connection;
 
 var kittySchema = mongoose.Schema({
@@ -16,7 +16,7 @@ function start(request, response) {
 		'charset=UTF-8" />'+
 		'</head>'+
 		'<body>'+
-		'<form action="/upload" method="post" enctype="multipart/form-data">'+
+		'<form action="/upload" method="post" >'+
 		'<input type="input" name="name" />'+
 		'<input type="submit" value="Submit Input" />'+
 		'</form>'+
@@ -31,17 +31,39 @@ function list(request, response) {
 	console.log('Request handler for "list" was called ');
 	var cats = Kitten.find().exec(function(err, result) {
 		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write(JSON.stringify(result));
+		var foo = '';
+		result.forEach(function(item, index, array) {
+			console.log(item);
+			foo += '<div>' + item.name + '</div>';
+		});
+		response.write(foo);
 		response.end();
 	});
 }
 
 function upload(request, response) {
 	console.log('Request handler for "add" was called ');
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write("done");
-	response.end();
+	if (request.method == 'POST') {
+		var body = '';
+		request.setEncoding('utf8');
+		request.addListener('data', function(data) {
+			body += data;
+		});
+		request.addListener('end', function() {
+			var j = qs.parse(body);
+			var bob = new Kitten({name: j.name});
+			bob.save(function(err, data) {
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.write("saved");
+				response.end();
+			});
+		});
+	} else {
+		response.writeHead(404, {'Content-Type': 'text/html'});
+		response.write("eh, 404, dude");
+		response.end();
 
+	}
 }
 
 exports.start = start;
